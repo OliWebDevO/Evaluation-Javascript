438bfa842c539c01f26af7994fcc2e492d8db0bf
OliWebDevO <oliver-vdb@hotmail.com>
2024-03-26 15:37:29 +0100

    the end

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ let table = []
 
 // déclaration de fonction visant à cacher le panier s'il est vide, le montrer s'il dispose d'un item
 
-const removePanier = ()=> {
+const ifEmptyRemovePanier = ()=> {
     if (panierMatchs.innerHTML === "") {
         panier.classList.add('hidden')
         
@@ -34,9 +34,15 @@ const numberOfBets = () => {
     nBets.innerHTML = panierMatchs.childElementCount
 }
 
+// fonction qui ajuste la cote totale en divisant la cote la totale par la cote de l'élement supprimé. Cette fonction est à placer dans une fonction ou un élément est supprimé
+
+const adjustCote = (coteOfRemovedItem)=> {
+    cote.innerHTML = `${(parseFloat(cote.innerHTML)/parseFloat(coteOfRemovedItem.innerHTML)).toFixed(2)}`
+}
+
 // Fonction pour passer en DarkMode
 
-function enableDarkMode() {
+const enableDarkMode = () => {
     document.querySelector('.bets-main').classList.add('dark-mode');
     localStorage.setItem('darkMode', 'true');
     lightDark.innerHTML = `<i class="fa-solid fa-moon" style="color: #ffffff;"></i>`
@@ -44,7 +50,7 @@ function enableDarkMode() {
 
 // Fonction pour désactiver le DarkMode
 
-function disableDarkMode() {
+const disableDarkMode = () => {
     document.querySelector('.bets-main').classList.remove('dark-mode');
     localStorage.setItem('darkMode', 'false');
     lightDark.innerHTML =  `<a class="light" href=""><i class="fa-solid fa-sun" style="color: #ffffff;"></i></a>`
@@ -93,10 +99,11 @@ fetch(`./scripts/datas.json`)
     betsTable.addEventListener('click', (event)=> {
         let i = parseInt(event.target.getAttribute('data-index'))
         if (event.target.classList.contains('home')) {
+            // Si on choisi l'équipe à domicile, on envoie ces infos dans le panier shop :
             panierMatchs.innerHTML += 
-                `<div class="panier-bet">
+                `<div data-index="${i}" class="panier-bet">
                     <div class = "choix"> 
-                        <div data-index="${i} class="choisi">
+                        <div data-index="${i}" class="choisi">
                             <a  data-index="${i}">${table[i].hometeam}</a>
                             <a  data-index="${i}" class = "button">${table[i].home_odd}</a>
                         </div>
@@ -108,11 +115,17 @@ fetch(`./scripts/datas.json`)
                     <a  data-index="${i}">${table[i].awayteam}</a>
                     </div> 
                 </div>`
+            // on réinitialise les gains
+            gain.innerHTML = "0"
+            // On multiplie la cote existante par la cote relative à l'équipe à domicile
             cote.innerHTML = `${(parseFloat(cote.innerHTML)*table[i].home_odd).toFixed(2)}`
-        } else if (event.target.classList.contains('draw')) { panierMatchs.innerHTML += 
-                `<div class="panier-bet">
+            
+        } else if (event.target.classList.contains('draw')) {
+            // Si on choisi l'égalité, on envoie ces infos dans le panier shop :
+             panierMatchs.innerHTML += 
+                `<div data-index="${i}" class="panier-bet">
                     <div class = "choix"> 
-                        <div data-index="${i} class="choisi">
+                        <div data-index="${i}" class="choisi">
                             <a  data-index="${i}"> Draw </a>
                             <a  data-index="${i}" class = "button">${table[i].draw_odd}</a>
                         </div>
@@ -123,13 +136,17 @@ fetch(`./scripts/datas.json`)
                     <a>-</a> 
                     <a  data-index="${i}">${table[i].awayteam}</a>
                     </div>
-                </div>`  
-            cote.innerHTML = `${(parseFloat(cote.innerHTML)*table[i].draw_odd).toFixed(2)}`
+                </div>`
+                // on réinitialise les gains
+                gain.innerHTML = "0"  
+                // On multiplie la cote existante par la cote relative à l'égalité
+                cote.innerHTML = `${(parseFloat(cote.innerHTML)*table[i].draw_odd).toFixed(2)}`
             } else if (event.target.classList.contains('away')) {
+                // Si on choisi l'équipe extérieure, on envoie ces infos dans le panier shop :
                 panierMatchs.innerHTML += 
-                    `<div class="panier-bet">
+                    `<div data-index="${i}" class="panier-bet">
                         <div class = "choix"> 
-                            <div data-index="${i} class="choisi">
+                            <div data-index="${i}" class="choisi">
                                 <a  data-index="${i}">${table[i].awayteam}</a>
                                 <a  data-index="${i}" class = "button">${table[i].away_odd}</a>
                             </div>
@@ -141,10 +158,15 @@ fetch(`./scripts/datas.json`)
                         <a  data-index="${i}"> ${table[i].awayteam}</a>
                         </div> 
                     </div>`  
-                cote.innerHTML = `${(parseFloat(cote.innerHTML)*table[i].away_odd).toFixed(2)}`
+                    // on réinitialise les gains
+                    gain.innerHTML = "0"
+                    // On multiplie la cote existante par la cote relative à l'équipe extérieure
+                    cote.innerHTML = `${(parseFloat(cote.innerHTML)*table[i].away_odd).toFixed(2)}`
             }
+            // On vérifie le nombre de paris présent et on update le nombre
             numberOfBets()
-            removePanier()
+            // On vérifie s'il y a des élément dans le panier, s'il n'y en a pas on retire le panier
+            ifEmptyRemovePanier()
     });  
              
 })
@@ -158,37 +180,90 @@ fetch(`./scripts/datas.json`)
 
 // On appelle la fonction pour cacher le panier au chargement de la page
 
- removePanier()
+    ifEmptyRemovePanier()
 
 // Event visant a toggle active/na les cotes dans le betsTable, via un foreach on veille à ce que le toggle ne se fasse que ligne par ligne
 
     betsTable.addEventListener('click', (event) => {
-        // Check if the clicked element is a button within a section
+        // On verifie que l'élement ai la classe button et qu'il soit proche de bet
         if (event.target.classList.contains('button') && event.target.closest('.bet')) {
             let sectionContainer = event.target.closest('.bet');
-            // Toggle the 'active' class on the clicked button
+            // on active la classe active
             event.target.classList.toggle('active');
             
-            // Remove the 'active' class from other buttons within the same section
+            // on retire la classe active des autres .button dans la même section
             let buttons = sectionContainer.querySelectorAll('.button');
             buttons.forEach(button => {
                 if (button !== event.target && button.classList.contains('active')) {
                     button.classList.remove('active');
                 }
+                
             });
+            // Si on clique sur un bouton avec classe actif :
+            // On verifie qu'il n'y ai pas deux fois le même index de match afin d'éviter de pouvoir avoir plusieurs paris sur le même match
+            if (event.target.classList.contains('active')) {
+                let i = parseInt(event.target.getAttribute('data-index'));
+                let panierBets = document.querySelectorAll('.panier-bet');
+                panierBets.forEach(panierBet => {
+                    if (parseInt(panierBet.getAttribute('data-index')) === i) {
+                        
+                        panierBet.remove();
+                        // on réinitialise les gains
+                        gain.innerHTML = "0"
+                // On retire la cote précédente afin d'avoir uniquement la dernière cote ajoutée
+                        let coteOfRemovedItem = panierBet.querySelector('.button')
+                        adjustCote(coteOfRemovedItem)
+
+                    }
+                });
+            }
+            // Si on clique sur un bouton sans classe actif :
+            // On verifie qu'il n'y ai pas deux fois le même index de match afin d'éviter de pouvoir avoir plusieurs paris sur le même match
+            if (!event.target.classList.contains('active')) {
+                let i = parseInt(event.target.getAttribute('data-index'));
+                let panierBets = document.querySelectorAll('.panier-bet');
+                panierBets.forEach(panierBet => {
+                    if (parseInt(panierBet.getAttribute('data-index')) === i) {
+                        
+                         panierBet.remove();
+                         // on réinitialise les gains
+                         gain.innerHTML = "0"
+                // On retire la cote précédente afin d'avoir uniquement la dernière cote ajoutée
+                        let coteOfRemovedItem = panierBet.querySelector('.button')
+                        adjustCote(coteOfRemovedItem)
+                    }
+                });
+            }
+            
         }
     });
-
+    
 // Event ciblant l'icone delete du panier, supprime la div correspondante et déduis également la cote totale du panier.
 
-panierMatchs.addEventListener('click', (e) => {
-    if (e.target.classList.contains('delete')) {
-        let coteOfRemovedItem= e.target.previousElementSibling.querySelector('.button')
-        cote.innerHTML = `${(parseFloat(cote.innerHTML)/parseFloat(coteOfRemovedItem.innerHTML)).toFixed(2)}`
-        e.target.parentElement.parentElement.remove()
+panierMatchs.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete')) {
+        let coteOfRemovedItem= event.target.previousElementSibling.querySelector('.button')
+        // On retire la cote précédente afin d'avoir uniquement la dernière cote ajoutée
+        adjustCote(coteOfRemovedItem)
+        // Quand on retire un élément du shop via la poubelle, on désactive le bouton correspondant dans la section betsTable
+        let index = parseInt(event.target.parentElement.parentElement.getAttribute('data-index'))
+        let buttons = document.querySelectorAll('.button');
+        buttons.forEach(button => {
+            if (parseInt(button.getAttribute('data-index')) === index && button.classList.contains('active')) {
+                button.classList.remove('active');
+            }
+        });
+        // On retire l'élement
+        event.target.parentElement.parentElement.remove()
+        // on réinitialise les gains
+        gain.innerHTML = "0"
+      
+        // if ()
     }
+        // On vérifie le nombre de paris présent et on update le nombre
         numberOfBets()
-        removePanier()
+        // On vérifie s'il y a des élément dans le panier, s'il n'y en a pas on retire le panier
+        ifEmptyRemovePanier()
 })
 
 // Event de click visant à estimer les gains en fonction de la cote et de la somme donnée par l'utilisateur
